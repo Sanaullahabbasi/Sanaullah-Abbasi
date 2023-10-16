a6475e864e8fd16ef2b517c186fafbb420416338
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-10-16 21:19:53 +0500

    Mobile Mall assignment

---
+++ JavaScript/20th Assignment (JS 10th)/app.js
@@ -0,0 +1,412 @@
+var mobile = {
+  apple: {
+    iPhone14ProMax: {
+      name: "iPhone 14 Pro Max",
+      price: 565000,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone14ProMax-b.jpg",
+    },
+    iPhone14Pro: {
+      name: "iPhone 14 Pro",
+      price: 518999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone14Pro-b.jpg",
+    },
+
+    iPhone14Plus: {
+      name: "iPhone 14 Plus",
+      price: 422999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone14Plus-b.jpg",
+    },
+    iPhone14: {
+      name: "iPhone 14",
+      price: 384999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone14-b.jpg",
+    },
+    iPhone13ProMax: {
+      name: "iPhone 13 Pro Max",
+      price: 373999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone13ProMax-b.jpg",
+    },
+    iPhone13Pro: {
+      name: "iPhone 13 Pro",
+      price: 342999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone13Pro-b.jpg",
+    },
+    iPhone13: {
+      name: "iPhone 13",
+      price: 273899,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone13-b.jpg",
+    },
+    iPhone12ProMax: {
+      name: "iPhone 12 Pro Max",
+      price: 236999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone12ProMax-b.jpg",
+    },
+    iPhone11ProMax: {
+      name: "iPhone 11 Pro Max",
+      price: 384999,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "4/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone11ProMax-b.jpg",
+    },
+    iPhone13Mini: {
+      name: "iPhone 13 Mini",
+      price: 223399,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "4/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone13Mini-b.jpg",
+    },
+    iPhone12Pro: {
+      name: "iPhone 12 Pro",
+      price: 220899,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone12Pro-b.jpg",
+    },
+    iPhone11Pro: {
+      name: "iPhone 11 Pro",
+      price: 207799,
+      color: ["Space Black", "Silver", "Gold", "Deep Purple"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/AppleiPhone11Pro-b.jpg",
+    },
+  },
+  oppo: {
+    OppoReno6Pro: {
+      name: "Oppo Reno 6 Pro",
+      price: 119999,
+      color: ["Stellar Black", "Aurora"],
+      memory: "12/256GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoReno6Pro-b.jpg",
+    },
+    OppoF21Pro5G: {
+      name: "Oppo F21 Pro 5G",
+      price: 104999,
+      color: ["Rainbow Spectrum", "Cosmic Black"],
+      memory: "8/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoF21Pro5G-b.jpg",
+    },
+    OppoF21Pro: {
+      name: "Oppo F21 Pro",
+      price: 89999,
+      color: ["Cosmic Black", "Sunset Orange"],
+      memory: "8/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoF20Pro-b.jpg",
+    },
+    OppoReno5Pro: {
+      name: "Oppo Reno 5 Pro",
+      price: 84999,
+      color: ["Galactic Silver", "Starry Black"],
+      memory: "12/256GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoReno5Pro-b.jpg",
+    },
+    OppoA96: {
+      name: "Oppo A96",
+      price: 77999,
+      color: ["Stary Black", "sunset Blue"],
+      memory: "8/256GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoA96-b.jpg",
+    },
+    OppoA77s: {
+      name: "Oppo A77s",
+      price: 74999,
+      color: ["Sunset Orange", "Starry Black"],
+      memory: "8/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/OppoA77s-b.jpg",
+    },
+  },
+  Huawei: {
+    HuaweiP30Pro: {
+      name: "Huawei P30 Pro",
+      price: 157600,
+      color: ["Aurora", "Amber Sunrise", "Breathing Crystal", "Black"],
+      memory: "8/256GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/HuaweiP30Pro-b.jpg",
+    },
+    HuaweiMate20Pro: {
+      name: "Huawei Mate 20 Pro",
+      price: 119999,
+      color: [
+        "Emerald Green",
+        "Midnight Blue",
+        "Twilight",
+        "Pink Gold",
+        "Black",
+      ],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/HuaweiMate20Pro-b.jpg",
+    },
+    HuaweiNova9: {
+      name: "Huawe iNova 9",
+      price: 107999,
+      color: ["Starry Blue", "Black", "Green", "Violet"],
+      memory: "8/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/HuaweiNova9-b.jpg",
+    },
+    HuaweiP20Pro: {
+      name: "Huawei P20 Pro",
+      price: 99999,
+      color: ["Black", "Blue", "Twilight"],
+      memory: "6/128GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/HuaweiP20Pro-b.jpg",
+    },
+    HuaweiNova8Pro: {
+      name: "Huawei Nova 8 Pro",
+      price: 97999,
+      color: ["Black", "Blue", "Green", "White"],
+      memory: "8/256GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/HuaweiNova8Pro-b.jpg",
+    },
+  },
+  motorola: {
+    MotorolaMotoOneMacro: {
+      name: "Motorola Moto One Macro",
+      price: 29999,
+      color: ["Space Blue"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaOneMacro-b.jpg",
+    },
+    MotorolaMotoE6: {
+      name: "Motorola Moto E6",
+      price: 24999,
+      color: ["Gray"],
+      memory: "2/32GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6-b.jpg",
+    },
+    MotorolaMotoE6Plus: {
+      name: "Motorola Moto E6 Plus",
+      price: 19999,
+      color: ["Polished Graphite", "Bright Cherry", "Dark Red"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6Plus-b.jpg",
+    },
+    MotorolaMotoE3: {
+      name: "Motorola Moto E3",
+      price: 9999,
+      color: ["Black", "White"],
+      memory: "2/16GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoE3Powerb.jpg",
+    },
+    MotorolaMotoG5Plus: {
+      name: "Motorola Moto G5 Plus",
+      price: 18999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "2/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/G5Plusb.jpg",
+    },
+    MotorolaMotoXForce: {
+      name: "Motorola Moto X Force",
+      price: 16999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "3/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoXForceb.jpg",
+    },
+  },
+  realme: {
+    MotorolaMotoOneMacro: {
+      name: "Motorola Moto One Macro",
+      price: 29999,
+      color: ["Space Blue"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaOneMacro-b.jpg",
+    },
+    MotorolaMotoE6: {
+      name: "Motorola Moto E6",
+      price: 24999,
+      color: ["Gray"],
+      memory: "2/32GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6-b.jpg",
+    },
+    MotorolaMotoE6Plus: {
+      name: "Motorola Moto E6 Plus",
+      price: 19999,
+      color: ["Polished Graphite", "Bright Cherry", "Dark Red"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6Plus-b.jpg",
+    },
+    MotorolaMotoE3: {
+      name: "Motorola Moto E3",
+      price: 9999,
+      color: ["Black", "White"],
+      memory: "2/16GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoE3Powerb.jpg",
+    },
+    MotorolaMotoG5Plus: {
+      name: "Motorola Moto G5 Plus",
+      price: 18999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "2/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/G5Plusb.jpg",
+    },
+    MotorolaMotoXForce: {
+      name: "Motorola Moto X Force",
+      price: 16999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "3/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoXForceb.jpg",
+    },
+  },
+  techno: {
+    MotorolaMotoOneMacro: {
+      name: "Motorola Moto One Macro",
+      price: 29999,
+      color: ["Space Blue"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaOneMacro-b.jpg",
+    },
+    MotorolaMotoE6: {
+      name: "Motorola Moto E6",
+      price: 24999,
+      color: ["Gray"],
+      memory: "2/32GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6-b.jpg",
+    },
+    MotorolaMotoE6Plus: {
+      name: "Motorola Moto E6 Plus",
+      price: 19999,
+      color: ["Polished Graphite", "Bright Cherry", "Dark Red"],
+      memory: "4/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotorolaMotoE6Plus-b.jpg",
+    },
+    MotorolaMotoE3: {
+      name: "Motorola Moto E3",
+      price: 9999,
+      color: ["Black", "White"],
+      memory: "2/16GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoE3Powerb.jpg",
+    },
+    MotorolaMotoG5Plus: {
+      name: "Motorola Moto G5 Plus",
+      price: 18999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "2/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/G5Plusb.jpg",
+    },
+    MotorolaMotoXForce: {
+      name: "Motorola Moto X Force",
+      price: 16999,
+      color: ["Lunar Grey", "Fine Gold"],
+      memory: "3/64GB",
+      sim: "Dual SIM (Nano-SIM)",
+      Image: "Images/MotoXForceb.jpg",
+    },
+  },
+};
+
+var company = document.getElementById("company");
+var model = document.getElementById("model");
+var allMobile = document.getElementById("allMobile");
+
+company.innerHTML = `<option value="">Select Company</option>`;
+
+for (var key in mobile) {
+  company.innerHTML += `
+  <option value="${key}">${key.toUpperCase()}</option>
+  `;
+  for (var key1 in mobile[key]) {
+    var allData = mobile[key][key1];
+    var colorDiv = "";
+    for (var i = 0; i < allData.color.length; i++) {
+      console.log(allData.color.length)
+      allData += `<div style="widht:10px; height: 10px; border-radius: 5px; background-color: ${allData.color[i]}"></div>`;
+    }
+    allMobile.innerHTML += `
+    <div  class="col mb-2">
+        <div class="card">
+        <img src="${allData.Image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${allData.name}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${allData.memory}</h6>
+            <h2>Rs ${allData.price}</h2> 
+            
+            <div class="color">
+            <div></div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+function onCompanyChange() {
+  model.innerHTML = "";
+  model.innerHTML = `<option value="">Select Model</option>`;
+  for (var key in mobile[company.value]) {
+    model.innerHTML += `
+<option value="${key}">${key.toUpperCase()}</option>
+`;
+  }
+}
+
+function filterMobiles() {
+  var mobileData = mobile[company.value][model.value];
+  allMobile.innerHTML = `
+  <div  class="col mb-2">
+      <div class="card">
+      <img src="${mobileData.Image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${mobileData.name}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">${mobileData.memory}</h6>
+          <h2>Rs ${mobileData.price}</h2>           
+        </div>
+      </div>
+    </div>
+  `;
+}
