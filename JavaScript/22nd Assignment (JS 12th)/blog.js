36eb427ee4298d633175a64316dbc017ed97adcd
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-08 20:26:34 +0500

    quill rich text formating updated

--- JavaScript/22nd Assignment (JS 12th)/blog.js
+++ JavaScript/22nd Assignment (JS 12th)/blog.js
@@ -34,11 +34,34 @@ function writeBlog() {
   </div>
 </div>
   `;
+  
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+
   var quill = new Quill('#editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
     theme: 'snow'
   });
   myData = quill
-
   document.getElementById("files").disabled = true;
 }
 
@@ -87,7 +110,7 @@ function postArticle() {
       <div class="card-body">
         <h5 class="card-title">${title.value}</h5>
         <p class="card-text" id="para">
-          ${myData.getText()}
+          ${myData.root.innerHTML}
         </p>
         <a id="link" href="Javascript:void(0)" onclick="expandText()" class="btn btn-primary">read more</a>
       </div>
