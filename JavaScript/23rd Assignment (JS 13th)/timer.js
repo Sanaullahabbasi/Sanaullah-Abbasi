08569a53a4bee30230babf03460085b8c311002b
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-11 23:12:20 +0500

    updated

--- JavaScript/23rd Assignment (JS 13th)/timer.js
+++ JavaScript/23rd Assignment (JS 13th)/timer.js
@@ -1,4 +1,3 @@
-
 var timerValue = document.getElementById("timer_value");
 var sec = document.getElementById("sec");
 var min = document.getElementById("min");
@@ -9,17 +8,16 @@ function startTimer() {
   var minValue = parseInt(getValue.slice(0, 1));
   var secValue = parseInt(getValue.slice(1, 3));
   var minV = parseInt(getValue.slice(0, 2));
-  var secV = parseInt(getValue.slice(2, 4)); 
-
+  var secV = parseInt(getValue.slice(2, 4));
+  timerValue.style.display = "none";
   function countDown() {
     if (timerValue.value.length <= 2) {
       getValue--;
       sec.innerHTML = getValue + `<span>s</span>`;
-      if(getValue <1){
-        sec.innerHTML = `<h1>Time Out</h1>`
+      if (getValue < 1) {
+        sec.innerHTML = `<h1>Time Out</h1>`;
       }
     } else if (timerValue.value.length === 3) {
-
       if (secValue < 1) {
         secValue = 59;
         minValue--;
@@ -28,31 +26,28 @@ function startTimer() {
         secValue--;
         console.log(secValue);
       }
-      
+
       sec.innerHTML = secValue + `<span>s</span>`;
       min.innerHTML = minValue + `<span>m</span>`;
-      if(minValue < 1 && secValue < 1){
+      if (minValue < 1 && secValue < 1) {
         sec.innerHTML = `<h1>Time Out</h1>`;
-        clearInterval(timer); 
+        clearInterval(timer);
+        min.innerHTML = ``;
       }
     } else if (timerValue.value.length === 4) {
-      var getMin = timerValue.value;
-      var getSec = timerValue.value;
-     
       if (secV < 1) {
         secV = 59;
         minV--;
-        console.log(minV);
-      } else {
+      } else  {
         secV--;
-        console.log(secV);
+      }
+      if (minV < 1 && secV < 1) {
+        sec.innerHTML = `<h1>Time Out</h1>`;
+        clearInterval(timer);
+        min.innerHTML = ``;
       }
       sec.innerHTML = secV + `<span>s</span>`;
       min.innerHTML = minV + `<span>m</span>`;
-      if(minValue < 1 && secValue < 1){
-        sec.innerHTML = `<h1>Time Out</h1>`;
-        clearInterval(timer); 
-      }
     }
   }
 
@@ -60,7 +55,9 @@ function startTimer() {
 }
 
 function resetTimer() {
-  clearInterval(timer);  
-  sec.innerHTML = 0 + `<span>s</span>`;
-  min.innerHTML = `<span></span>`;
+  clearInterval(timer);
+  timerValue.style.display = "block";
+  timerValue.value = "";
+  sec.innerHTML = ``;
+  min.innerHTML = ``;
 }
